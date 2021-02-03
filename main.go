4d0610a5e4bc09434780d313512b7496960559a6
raindevops <raindevops.durand@gmail.com>
2021-02-03 19:44:19 +0100

    magic number function return value³

--- main.go
+++ main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"io"
+	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World!")
 }
 
 func getName(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Get name func")
+	v := mux.Vars(r)
+	io.WriteString(w, v["name"])
 }
 
 func magicNumber(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Magic number function")
+	v := mux.Vars(r)
+	n, err := strconv.Atoi(v["num"])
+	if err != nil {
+		io.WriteString(w, "Please enter a valide number")
+	} else {
+		mn := math.Pow(float64(n), 3)
+		io.WriteString(w, strconv.Itoa(int(mn)))
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", greet)
+	r.HandleFunc("/", home)
 	r.HandleFunc("/user/{name}", getName)
 	r.HandleFunc("/magic-number/{num}", magicNumber)
 
